7213f8a5a15c495a502fc1bb7dd1b067ed7f4d6a
npole0103 <npole0103@naver.com>
2021-06-05 00:09:39 +0900

    TCP/IP

---
+++ mine/Assets/Scripts/NetClient.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Unity.Network;
+using UnityEngine;
+
+namespace Unity.Network
+{
+    public class NetClient
+    {
+        private const string UserName = "UnityClient";
+
+        private LocalClient _client;
+
+        public event ReceiveMessage OnReceiveMessage;
+
+        public NetClient()
+        {
+
+        }
+
+        public void Start()
+        {
+            _client = new LocalClient();
+            _client.OnConnected += Connected;
+            _client.OnReceiveObject += ReceiveObject;
+            _client.Start();
+        }
+
+        public void Close()
+        {
+            _client.Close();
+        }
+
+        private void Connected() //커넥트
+        {
+            Connect connect = new Connect()
+            {
+                Type = PacketType.Connect,
+                UserName = UserName,
+            };
+
+            _client.UserName = UserName;
+            _client.SendPacket(connect); //패킷 서버에 전송
+        }
+
+        private void ConnectError(Exception ex)
+        {
+            Debug.Log("접속 에러\n" + ex.ToString());
+        }
+
+        private void ReceiveObject(Packet packet) //메시지 패킷 수신
+        {
+            if (packet == null)
+                return;
+
+            switch (packet.Type)
+            {
+                case PacketType.Message:
+                    ReceiveMessage(packet);
+                    break;
+            }
+        }
+
+        private void ReceiveMessage(Packet packet)
+        {
+            Message message = packet as Message;
+            if (message == null)
+                return;
+
+            if (OnReceiveMessage != null)
+                OnReceiveMessage(message.Content);
+        }
+
+        public void SendMessage(string message)
+        {
+            Message packet = new Message()
+            {
+                Content = message,
+            };
+            SendPacket(packet);
+        }
+
+        private void SendPacket(Packet packet)
+        {
+            _client.SendPacket(packet);
+        }
+    }
+
+    public delegate void ReceiveMessage(string message);
+
+}
---
+++ mine/Assets/Scripts/Runner.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using UnityEngine;
+using Unity.Network;
+
+public class Runner : MonoBehaviour
+{
+    private NetClient _netClient;
+
+    private string _message = "";
+    private string _sendMessage = "";
+    Vector2 ScrollPos;
+
+    private void Awake()
+    {
+        _netClient = new NetClient();
+        _netClient.OnReceiveMessage += ReceiveMessage;
+        _netClient.Start();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(400, 0, 340, 90));
+
+        GUILayout.TextArea(_message, GUILayout.Width(340), GUILayout.Height(40));
+        _sendMessage = GUILayout.TextField(_sendMessage, GUILayout.Width(340), GUILayout.Height(20));
+
+        if (GUILayout.Button("전송하기"))
+        {
+            if (_sendMessage.Length > 0)
+            {
+                _message = "관리자에게 보냄 : " + _sendMessage + "\n";
+                _netClient.SendMessage(_sendMessage);
+                _sendMessage = "";
+            }
+        }
+        
+        GUILayout.EndArea();
+    }
+
+    private void ReceiveMessage(string message)
+    {
+        _message = "받은 메세지 : " + message + "\n";
+    }
+
+    private void OnApplicationQuit()
+    {
+        try
+        {
+            _netClient.Close();
+        }
+        catch { }
+    }
+}
